424c0a85d5fea2015386bca44f96d23212c522c6
mega.it <mega.it@mega.it>
2023-09-20 18:33:14 +0900

    [김인규]  - 회원가입 암호화 기능  - 로그인 기본 기능  - 세션 활용 로그인 로그아웃 기능

--- src/main/java/com/marondal/memo/user/service/UserService.java
+++ src/main/java/com/marondal/memo/user/service/UserService.java
@@ -1,8 +1,11 @@
 package com.marondal.memo.user.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.marondal.memo.common.EncryptUtils;
 import com.marondal.memo.user.domain.User;
 import com.marondal.memo.user.repository.UserRepository;
 
@@ -12,15 +15,27 @@ public class UserService {
 	@Autowired
 	private UserRepository userRepository;
 	
+	public User getUser(String loginId, String password) {
+		
+		String encryptPassword = EncryptUtils.md5(password);
+		
+		Optional<User> optionalUser = userRepository.findByLoginIdAndPassword(loginId, encryptPassword);
+		User user = optionalUser.orElse(null);
+		
+		return user;
+	}
+	
 	public User addUser(
 			String loginId
 			, String password
 			, String name
 			, String email) {
 		
+		String encryptPassword = EncryptUtils.md5(password);
+		
 		User user = User.builder()
 						.loginId(loginId)
-						.password(password)
+						.password(encryptPassword)
 						.name(name)
 						.email(email)
 						.build();
