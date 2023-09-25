8b545e38d4b8e5501072957120185d5798b9be4a
mega.it <mega.it@mega.it>
2023-09-25 18:23:44 +0900

    [김인규]  - 메모 세부 화면  - 메모 파일 첨부 기능

--- src/main/java/com/marondal/memo/post/service/PostService.java
+++ src/main/java/com/marondal/memo/post/service/PostService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.marondal.memo.common.FileManager;
 import com.marondal.memo.post.domain.Post;
 import com.marondal.memo.post.repository.PostRepository;
 
@@ -14,9 +16,13 @@ public class PostService {
 	@Autowired
 	private PostRepository postRepository;
 	
-	public int addPost(int userId, String title, String content) {
+	public int addPost(int userId, String title, String content, MultipartFile file) {
 		
-		return postRepository.insertPost(userId, title, content);
+		// file을 특정 디렉토리(폴더)에 저장하고, 
+		// 저장된 파일을 접근할 수 있는 url 경로를 만들고 table 저장 
+		String imagePath = FileManager.saveFile(userId, file);
+		
+		return postRepository.insertPost(userId, title, content, imagePath);
 		
 	}
 	
@@ -25,5 +31,12 @@ public class PostService {
 		return postRepository.selectPostList(userId);
 		
 	}
+	
+	public Post getPost(int id) {
+		
+		return postRepository.selectPost(id);
+		
+	}
+	
 
 }
